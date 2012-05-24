9c07d0d4f9cc3533fb7a4b3610e3424095a53dee
Aurélien Pelletier <aurelien.pelletier@gmail.com>
2012-05-24 16:58:21 +0200

    Recette d'installation de tomcat

---
+++ cookbooks/tomcat/attributes/default.rb
@@ -0,0 +1,9 @@
+default['tomcat']['version'] = "7.0.22"
+default['tomcat']['tc7_download_url'] = "http://mirrors.ircam.fr/pub/apache/tomcat/tomcat-7/v7.0.27/bin/apache-tomcat-7.0.27.tar.gz"
+default['tomcat']['tc7_checksum'] = "38168d97dcd828698bb161fa7c051d8c229c7e9d609ee5e1132011f9fb68ec8e"
+default['tomcat']['user'] = "tomcat"
+default['tomcat']['uid'] = "1001"
+default['tomcat']['group'] = "tomcat"
+default['tomcat']['gid'] = "1001"
+default['tomcat']['catalinaHome'] = '/usr/local/tomcat'
+default['tomcat']['catalinaBase'] = '/etc/tomcat/catalinaBase'
---
+++ cookbooks/tomcat/recipes/default.rb
@@ -0,0 +1,111 @@
+include_recipe "java"
+
+catalinaHome = node['tomcat']['catalinaHome']
+catalinaBase = node['tomcat']['catalinaBase']
+installDir = catalinaHome[0,catalinaHome.rindex('/')]
+configFile = catalinaBase[0,catalinaBase.rindex('/')]+"/tomcat.conf"
+version = node['tomcat']['version']
+user = node['tomcat']['user']
+uid = node['tomcat']['uid']
+group = node['tomcat']['group']
+gid = node['tomcat']['gid']
+archiveFile= installDir+"/apache-tomcat.tar.gz"
+directoryName= "apache-tomcat-" + version
+absoluteDestinationPath= installDir + "/" + directoryName
+
+
+#Création de l'utilisateur tomcat
+group "#{group}" do
+  gid #{gid}
+  not_if "grep #{group} /etc/group"
+end
+
+user "#{user}" do
+  uid #{uid}
+  gid "#{user}"
+  home "/home/#{user}"
+  shell "/bin/bash"
+  not_if "grep #{user} /etc/passwd"
+end
+
+
+#Download et installation de tomcat
+remote_file "#{archiveFile}" do
+  source node['tomcat']['tc7_download_url']
+  checksum node['tomcat']['tc7checksum']
+  not_if "test -d #{absoluteDestinationPath} && test -d #{node.tomcat.catalinaHome}"
+end
+
+
+bash "untarTomcat" do
+  cwd "#{installDir}"
+  code <<-EOH
+	tar -xzf #{archiveFile}
+	rm #{archiveFile}
+	chown -R #{user}:#{group} #{directoryName}
+	rm #{node.tomcat.catalinaHome}
+	ln -s #{directoryName} tomcat
+  EOH
+  not_if "test -d #{absoluteDestinationPath} && test -d #{node.tomcat.catalinaHome}"
+end
+
+#installation de CATALINA_BASE
+["#{catalinaBase}","#{catalinaBase}/conf/Catalina/localhost","#{catalinaBase}/logs", "#{catalinaBase}/temp", "#{catalinaBase}/work", "#{catalinaBase}/webapp"].each do |dir|
+  directory dir do
+    owner "#{user}"
+    group "#{group}"
+    mode "0755"
+    recursive true
+    action :create
+  end
+end
+
+template "#{configFile}" do
+	source "tomcat_conf.erb"
+	owner "#{user}"
+	group "#{group}"
+	mode "0755"
+	variables({
+		:catalinaBase => "#{catalinaBase}",
+		:user => "#{user}"
+	})
+end
+
+execute "copyTomcatConf" do
+	user "#{user}"
+	group "#{group}"	  
+	command "cp -R #{absoluteDestinationPath}/conf #{catalinaBase}"
+end
+
+
+
+#Script de démarrage
+template "/etc/init.d/tomcat" do
+	source "tomcat_debian.erb"
+	mode "0755"
+	variables(
+	    :configFile => configFile
+	)
+end
+
+service "tomcat" do
+  supports :restart => true, :start => true, :stop => true, :reload => true
+  action [ :enable]
+end
+
+
+#tomcat user
+cookbook_file "#{catalinaBase}/conf/tomcat-users.xml" do
+  source "tomcat-users.xml"
+  mode "0644"
+  owner "#{user}"
+  group "#{group}"	 
+end
+
+# Ajout de la webapp manager
+cookbook_file "#{catalinaBase}/conf/Catalina/localhost/manager.xml" do
+	source "manager.xml"
+	mode "0655"
+	owner "#{user}"
+        group "#{group}"	 
+end
